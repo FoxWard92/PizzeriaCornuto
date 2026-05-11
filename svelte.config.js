13cb8a305da804f0724837f0fd601758280089dd
FoxWard92 <foxwardmine92@gmail.com>
2026-05-11 17:59:35 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		}),
 		paths: {
 			// DEVE corrispondere al nome del repo su GitHub
-			base: process.env.NODE_ENV === 'production' ? '/Pizzeria_Cornuto' : '',
+			base: process.env.NODE_ENV === 'production' ? '/PizzeriaCornuto' : '',
 		}
 	}
 };
